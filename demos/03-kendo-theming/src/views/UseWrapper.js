bec1410c2b2587df0750b2db46feb660d4a0dc6c
Milan Nankov <milan@nankov.com>
2018-07-09 23:08:08 +0300

    Deleted redundant files from Theming demo.

--- demos/03-kendo-theming/src/views/UseWrapper.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Scheduler } from '@progress/kendo-scheduler-react-wrapper';
-
-export default function UseWrapper() {
-    return (
-        <Scheduler/>
-    );
-}
