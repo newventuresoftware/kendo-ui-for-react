060906668a64196c160c1720a150ebba3e552588
Milan Nankov <milan@nankov.com>
2018-07-11 18:11:10 +0300

    Finished Components example.

--- demos/01-react/src/views/Components.js
+++ demos/01-react/src/views/Components.js
@@ -1,17 +1,40 @@
 import React from 'react';
 
+const countries = ["Austria", "Bulgaria", "Germany", "Greece", "Hungary", "United Kingdom", "Vatican City", "USA"];
+
 function Components() {
     return (
         <div>
-            <CustomComponent />
+            <ListComponent items={countries} title="Countries" />
         </div>
     );
 }
 
-function CustomComponent() {
-    return (
-        <span>CustomComponent</span>
-    );
+class ListComponent extends React.Component {
+
+    state = {
+        selectedItem: null
+    }
+
+    render() {
+        return (
+            <div className="listComponent">
+                <div>{this.props.title}:</div>
+                <ol>
+                    {this.props.items.map(country => (
+                        <li className={country === this.state.selectedItem ? "selected" : ""} 
+                            onClick={() => this.onClick(country)}>{country}</li>
+                    ))}
+                </ol>
+            </div>
+        );
+    }
+
+    onClick = (item) => {
+        this.setState({
+            selectedItem: item
+        });
+    }
 }
 
 export default Components;
