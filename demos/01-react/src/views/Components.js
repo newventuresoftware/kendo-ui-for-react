a5009321521c9c8f61ff5f58cc766eac703c0fa3
Milan Nankov <milan@nankov.com>
2018-07-12 13:55:05 +0300

    Updated Components demo for start.

--- demos/01-react/src/views/Components.js
+++ demos/01-react/src/views/Components.js
@@ -5,36 +5,18 @@ const numbers = [5, 10, 15, 9, 333];
 function Components() {
     return (
         <div>
-            <SumAndDouble numbers={numbers} />
+            <SumAndDouble />
         </div>
     );
 }
 
-class SumAndDouble extends React.Component {
-
-    constructor(props) {
-        super();
-
-        this.state = {
-            sum: props.numbers.reduce((sum, x) => sum + x)
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <div>Sum and Double</div>
-                <div>{this.state.sum}</div>
-                <button onClick={this.onDouble}>Double</button>
-            </div>
-        );
-    }
-
-    onDouble = (e) => {
-        this.setState({
-            sum: this.state.sum * 2
-        })
-    }
+function SumAndDouble() {
+    return (
+        <div>
+            <div>Sum and Double</div>
+            <button>Double</button>
+        </div>
+    );
 }
 
 export default Components;
